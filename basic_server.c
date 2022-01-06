57768e630415ebbe9735fb910da91923f4ff7354
Ryan <rypoon18@gmail.com>
2022-01-05 23:39:15 -0500

    add assignment

--- basic_server.c
+++ basic_server.c
@@ -1,10 +1,40 @@
-#include "pipe_networking.h"
+#include "socket_networking.h"
+
+void to_lower_case(char *str);
 
 
 int main() {
+    int listening = server_setup();
+    int client;
 
-  int to_client;
-  int from_client;
+    while (1) {
+      client = server_connect(listening);
+      printf("forking\n");
+      int f;
+      f = fork();
+      if (f) {
+        close(client);
+      }
+      else {
+        while (1) {
+          char buf[BUFFER_SIZE];
+          while (read(client, buf, sizeof(buf))) {
+            to_lower_case(buf);
+            write(client, buf, sizeof(buf));
+          }
+        }
+      }
+    }
 
-  from_client = server_handshake( &to_client );
+    return 0;
+}
+
+
+void to_lower_case(char *str) {
+  int i;
+  for (i = 0; i < strlen(str); i++) {
+    if (str[i] >= 65 && str[i] <= 90) {
+      str[i] += 32;
+    }
+  }
 }
--- socket_networking.h
+++ socket_networking.h
@@ -13,19 +13,11 @@
 
 #ifndef NETWORKING_H
 #define NETWORKING_H
-#define ACK "HOLA"
-#define WKP "mario"
 
-#define HANDSHAKE_BUFFER_SIZE 10
 #define BUFFER_SIZE 1000
 
-
-//for forking server
 int server_setup();
-int server_connect(int from_client);
-
-
-int server_handshake(int *to_client);
-int client_handshake(int *to_server);
+int server_connect();
+int client_connect();
 
 #endif
